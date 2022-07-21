296650f7f4a1d7fa98d2f5003c1626c3a9190e6c
Ilyas Seisov <ilyasseisov@gmail.com>
2022-07-21 06:15:29 -0700

    updated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
+// Variables
 const colors = ['#7b68ee', '#eb3dae', '#f7ce51', '#5bc5fa'];
 let colorNumber = 0;
-
 const button = document.querySelector('.change-color-btn');
 
-button.addEventListener('click', () => {
+// Functions
+const changeColor = () => {
   document.body.style.backgroundColor = colors[colorNumber];
-
   colorNumber < colors.length - 1 ? colorNumber++ : (colorNumber = 0);
-});
+};
+
+// Event Listeners
+button.addEventListener('click', changeColor);
